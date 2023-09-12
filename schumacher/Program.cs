38d3d432afa103a63ecc0b25eecae52a85c2f6c6
zeteny12 <zetus0512@gmail.com>
2023-09-12 13:21:04 +0200

    Schumi-foreach-lambda

---
+++ schumacher/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace schumacher
+{
+    internal class Program
+    {
+        static List<Schumi> adatok = new List<Schumi>();
+
+        static void Main(string[] args)
+        {
+            AdatokBeolvasasa();
+            Feladatok03();
+            Feladatok04();
+            Feladatok05();
+
+            Console.ReadKey();
+        }
+
+        private static void Feladatok05()
+        {
+            Console.WriteLine("5. feladat: Hibastatisztika");
+            foreach (var item in adatok.GroupBy(a => a.Status).Select(b => new {ok = b.Key, db=b.Count()}))
+            {
+                if (item.db > 2)
+                {
+                    Console.WriteLine("\t" + item.ok + " : " + item.db);
+                }                
+            }
+        }
+
+        private static void Feladatok04()
+        {
+            Console.WriteLine("4. feladat: Magyar Nagydíj helyezései");
+            foreach (var item in adatok.FindAll(a => a.Grandprix.Equals("Hungarian Grand Prix") && a.Position > 0))
+            {
+                Console.WriteLine("\t" + item.Date.ToShortDateString() + " : " + item.Position + ". hely");    
+            }
+        }
+
+        private static void Feladatok03()
+        {
+            Console.WriteLine("3. feladat: " + adatok.Count);
+        }
+
+        private static void AdatokBeolvasasa()
+        {
+            try {
+                using (StreamReader file = new StreamReader("schumacher.csv"))
+                {
+                    file.ReadLine();
+                    while (!file.EndOfStream)
+                    {
+                        adatok.Add(new Schumi(file.ReadLine()));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ schumacher/Schumi.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace schumacher
+{
+    internal class Schumi
+    {
+        DateTime date;
+        string grandprix;
+        int position;
+        int laps;
+        int points;
+        string team;
+        string status;
+
+        public DateTime Date { get => date; set => date = value; }
+        public string Grandprix { get => grandprix; set => grandprix = value; }
+        public int Position { get => position; set => position = value; }
+        public int Laps { get => laps; set => laps = value; }
+        public int Points { get => points; set => points = value; }
+        public string Team { get => team; set => team = value; }
+        public string Status { get => status; set => status = value; }
+
+        public Schumi(DateTime date, string grandprix, int position, int laps, int points, string team, string status)
+        {
+            Date = date;
+            Grandprix = grandprix;
+            Position = position;
+            Laps = laps;
+            Points = points;
+            Team = team;
+            Status = status;
+        }
+        
+        public Schumi(string BeolvasottSor)
+        {
+            string[] strings = BeolvasottSor.Split(';');
+            Date = DateTime.Parse(strings[0]);
+            Grandprix = strings[1];
+            Position = int.Parse(strings[2]);
+            Laps = int.Parse(strings[3]);
+            Points = int.Parse(strings[4]);
+            Team = strings[5];
+            Status = strings[6];
+        }
+    }
+}
